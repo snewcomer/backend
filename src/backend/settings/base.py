090c4eec4ec8669dcb54d2c1c5e5b62ef3308af3
snewcomer <snewcomer24@gmail.com>
2016-09-02 07:13:48 -0700

    small changes

--- src/backend/settings/base.py
+++ src/backend/settings/base.py
@@ -123,5 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
